02d7a4241699ff5321c90f8db2a9e0a3a78f52bc
Falusi András <andras.falusi@gmail.com>
2021-08-31 12:17:51 +0200

    html + css + react added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Texts from './components/texts/Texts';
+import Menu from './components/menu/Menu';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Texts/>
+      <Menu/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/menu/Menu.js
@@ -0,0 +1,13 @@
+import picture from './menu.png'
+import './Menu.css'
+
+function Menu() {
+
+    return(
+        <div>
+            <img src={picture} alt="menu"/>
+        </div>
+    )
+}
+
+export default Menu
